76726d34b3f1cb3e9cfbfee45087220d7ef7fc0d
Kassio Oliveira <kassiogo@gmail.com>
2025-11-01 22:50:52 -0300

    versão inicial

---
+++ domain/src/main/java/com/example/domain/entity/Person.java
@@ -0,0 +1,71 @@
+package com.example.domain.entity;
+
+public class Person {
+
+    private Long id;
+    private String name;
+    private Boolean active;
+
+    public Person() {
+    }
+
+    public Person(String name, Boolean active) {
+        this.name = name;
+        this.active = active;
+    }
+
+    public Person(Long id, String name, Boolean active) {
+        this.id = id;
+        this.name = name;
+        this.active = active;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
